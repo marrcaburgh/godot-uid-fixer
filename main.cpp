326ffff2e47b66c509faeb352818e2b701f32445
marrcaburgh <marrcaburgh@gmail.com>
2026-01-13 09:02:36 -0500

    Fixed bugs

--- main.cpp
+++ main.cpp
@@ -5,67 +5,66 @@
 #include <random>
 #include <dirent.h>
 
-const std::string charset {
+const std::string CHARACTER_SET {
         "abcdefghijklmnopqrstuvwxyz"
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         "0123456789"
     };
-const int string_length { 12 };
+const int STRING_LENGTH { 12 };
 
 std::string generateRandomUID() {
     std::string result {};
-    result.resize(string_length);
+    result.resize(STRING_LENGTH);
 
     std::random_device random_device {};
     std::mt19937 random_number_generator { random_device() };
-    std::uniform_int_distribution<int> int_distribution { 0, static_cast<int>(charset.length() - 1) };
+    std::uniform_int_distribution<int> int_distribution { 0, static_cast<int>(CHARACTER_SET.length() - 1) };
 
-    for (int i = 0; i < string_length; i++) {
-        result[i] = charset[int_distribution(random_number_generator)];
+    for (int i = 0; i < STRING_LENGTH; i++) {
+        result[i] = CHARACTER_SET[int_distribution(random_number_generator)];
     }
 
     return result;
 }
 
-void handleFile(struct dirent* ent) {
-    if (+ent->d_name == +"." || +ent->d_name == +"..") {
-        return;
+bool handleFile(struct dirent* ent) {
+    std::filesystem::path file_path { ent->d_name };
+
+    if (file_path.extension() != ".tres") {
+        return true;
     }
 
-    std::filesystem::path filePath { ent->d_name };
-
-    if (filePath.extension() != ".tres") {
-        return;
-    }
-
-    std::ifstream input_file_stream { filePath };
+    std::ifstream input_file_stream { file_path };
 
     if (!input_file_stream.is_open()) {
-        return;
+        std::cerr << "ERROR: Unable to open file: " << file_path << " (Maybe invalid read/write permissions?)\n";
+        return false;
     }
-
-    std::filesystem::path temp_file_path { filePath.string() + ".tmp" };
-    std::ofstream output_file_stream { temp_file_path };
+    
+    std::filesystem::path tempfile_path { file_path.string() + ".tmp" };
+    std::ofstream output_file_stream { tempfile_path };
 
     if (!output_file_stream.is_open()) {
-        return;
+        std::cerr << "ERROR: Unable to open temp file: " << tempfile_path << " (Maybe invalid read/write permissions?)\n";
+        return false;
     }
 
-    std::cout << "Reading file: " << filePath << '\n';
+    std::cout << "Reading file: " << file_path << '\n';
     std::string line {};
 
     while (std::getline(input_file_stream, line)) {
-        size_t uid_position { line.find("uid://") };
+        size_t uid_position { line.find("\"uid://") };
 
         if (uid_position == std::string::npos) {
             output_file_stream << line << '\n';
             continue;
         }
 
-        uid_position += 6;
+        uid_position += 7;
         
         std::cout << "line: " << line << '\n';
-        line.replace(uid_position, string_length, generateRandomUID());
+        size_t endquote_pos { line.find('"', uid_position) };
+        line.replace(uid_position, endquote_pos - uid_position, generateRandomUID());
         std::cout << "new line: " << line << '\n';
 
         output_file_stream << line << '\n';
@@ -73,23 +72,28 @@ void handleFile(struct dirent* ent) {
 
     input_file_stream.close();
     output_file_stream.close();
-    std::remove(filePath.c_str());
-    std::rename(temp_file_path.c_str(), filePath.c_str());
+    std::remove(file_path.c_str());
+    std::rename(tempfile_path.c_str(), file_path.c_str());
+
+    return true;
 }
 
 int main() {
-    DIR* directory {};
-    struct dirent* ent {};
+    DIR* directory { opendir(".") };
 
-    directory = opendir(".");
     if (!directory) {
+        std::cerr << "ERROR: Unable to open current directory! (Maybe invalid read/write permissions?)\n";
         return -1;
     }
 
     std::cout << "Randomizing UIDs of all godot resources in current directory...\n";
 
+    struct dirent* ent {};
+
     while ((ent = readdir(directory)) != NULL) {
-        handleFile(ent);
+        if (!handleFile(ent)) {
+            return -2;
+        }
     }
 
     std::cout << "Done!\n";
