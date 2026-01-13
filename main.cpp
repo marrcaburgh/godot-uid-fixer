b76355fb558de5b5b7d4a8d87b8b8dd120d28ba9
marrcaburgh <marrcaburgh@gmail.com>
2026-01-13 11:42:09 -0500

    Added recursive and verbose options

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "CLI11.hpp"
 #include <fstream>
 #include <filesystem>
 #include <iostream>
@@ -5,7 +6,7 @@
 #include <random>
 #include <dirent.h>
 
-const double VERSION = 1.2;
+const double VERSION = 1.3;
 const std::string CHARACTER_SET {
         "abcdefghijklmnopqrstuvwxyz"
         "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -13,13 +14,16 @@ const std::string CHARACTER_SET {
     };
 const int STRING_LENGTH { 12 };
 
+bool verbose { false };
+bool recursive { false };
+
 std::string generateRandomUID() {
     std::string result {};
     result.resize(STRING_LENGTH);
 
     std::random_device random_device {};
-    std::mt19937 random_number_generator { random_device() };
-    std::uniform_int_distribution<int> int_distribution { 0, static_cast<int>(CHARACTER_SET.length() - 1) };
+    std::mt19937 random_number_generator(random_device());
+    std::uniform_int_distribution<int> int_distribution (0, static_cast<int>(CHARACTER_SET.length() - 1));
 
     for (int i = 0; i < STRING_LENGTH; i++) {
         result[i] = CHARACTER_SET[int_distribution(random_number_generator)];
@@ -28,30 +32,31 @@ std::string generateRandomUID() {
     return result;
 }
 
-bool handleFile(struct dirent* ent) {
-    std::filesystem::path file_path { ent->d_name };
-
+bool handleFile(std::filesystem::path file_path) {
     if (file_path.extension() != ".tres") {
         return true;
     }
 
-    std::ifstream input_file_stream { file_path };
+    std::cout << "File: " << file_path.filename() << '\n';
+    std::cout << "Path: " << file_path.parent_path() << '\n';
+
+    std::ifstream input_file_stream(file_path);
 
     if (!input_file_stream.is_open()) {
         std::cerr << "ERROR: Unable to open file: " << file_path << " (Maybe invalid read/write permissions?)\n";
         return false;
     }
     
-    std::filesystem::path tempfile_path { file_path.string() + ".tmp" };
-    std::ofstream output_file_stream { tempfile_path };
+    std::filesystem::path tempfile_path(file_path.string() + ".tmp");
+    std::ofstream output_file_stream(tempfile_path);
 
     if (!output_file_stream.is_open()) {
         std::cerr << "ERROR: Unable to open temp file: " << tempfile_path << " (Maybe invalid read/write permissions?)\n";
         return false;
     }
 
-    std::cout << "Reading file: " << file_path << '\n';
     std::string line {};
+    int line_count {};
 
     while (std::getline(input_file_stream, line)) {
         size_t uid_position { line.find("\"uid://") };
@@ -62,14 +67,25 @@ bool handleFile(struct dirent* ent) {
         }
 
         uid_position += 7;
+        size_t endquote_position { line.find('"', uid_position) };
+
+        if (verbose) {
+            std::cout << "Replacing line: " << line << '\n';
+            std::cout << "UID: " << line.substr(uid_position, endquote_position - uid_position) << '\n';
+        }
         
-        std::cout << "line: " << line << '\n';
-        size_t endquote_pos { line.find('"', uid_position) };
-        line.replace(uid_position, endquote_pos - uid_position, generateRandomUID());
-        std::cout << "new line: " << line << '\n';
+        line.replace(uid_position, endquote_position - uid_position, generateRandomUID());
+
+        if (verbose) {
+            std::cout << "Replaced line: " << line << '\n';
+            std::cout << "UID: " << line.substr(uid_position, endquote_position - uid_position) << '\n';
+        }
 
         output_file_stream << line << '\n';
+        line_count += 1;
     }
+
+    std::cout << "Wrote " << line_count << " line(s).\n";
 
     input_file_stream.close();
     output_file_stream.close();
@@ -79,28 +95,44 @@ bool handleFile(struct dirent* ent) {
     return true;
 }
 
-int main() {
-    std::cout << "godot-uid-fixer v" << VERSION << '\n';
-    DIR* directory { opendir(".") };
 
-    if (!directory) {
-        std::cerr << "ERROR: Unable to open current directory! (Maybe invalid read/write permissions?)\n";
-        return -1;
-    }
-    
-    std::cout << "Randomizing UIDs of all godot resources in current directory...\n";
 
-    struct dirent* ent {};
+int main(int argc, char **argv) {
+    CLI::App app("Randomizes UIDs of all godot resources in the current directory.");
 
-    while ((ent = readdir(directory)) != NULL) {
-        if (!handleFile(ent)) {
-            return -2;
+    app.add_flag("-v, --verbose", verbose, "verbosely randomize uids");
+    app.add_flag("-r, --recursive", recursive, "recursively randomize uids");
+
+    argv = app.ensure_utf8(argv);
+    CLI11_PARSE(app, argc, argv);
+
+    std::cout << "godot-uid-fixer v" << VERSION << "\n\n";
+
+    std::filesystem::path directory_path(".");
+
+    if (recursive) {
+        std::cout << "Randomizing UIDs of all godot resources in current directory and all subdirectories...\n";
+        std::filesystem::recursive_directory_iterator directory_iterator(directory_path);
+        for (const std::filesystem::directory_entry& entry : directory_iterator) {
+            if (entry.is_regular_file()) {
+                if (!handleFile(entry.path())) {
+                  return -2;
+                }
+            }
+        }
+    } else {
+        std::cout << "Randomizing UIDs of all godot resources in current directory...\n";
+        std::filesystem::directory_iterator directory_iterator(directory_path);
+        for (const std::filesystem::directory_entry& entry : directory_iterator) {
+            if (entry.is_regular_file()) {
+                if (!handleFile(entry.path())) {
+                    return -2;
+                }
+            }
         }
     }
 
     std::cout << "Done!\n";
-
-    closedir(directory);
 
     return 0;
 }
