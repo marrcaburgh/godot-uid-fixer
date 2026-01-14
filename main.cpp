43ba578dc01f20d1325e67f85531eda538691715
marrcaburgh <marrcaburgh@gmail.com>
2026-01-13 20:29:54 -0500

    Code cleanup

--- main.cpp
+++ main.cpp
@@ -1,138 +1,202 @@
 #include "CLI11.hpp"
-#include <fstream>
-#include <filesystem>
-#include <iostream>
-#include <string>
-#include <random>
 #include <dirent.h>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <random>
+#include <string>
+#include <vector>
 
-const double VERSION = 1.3;
-const std::string CHARACTER_SET {
-        "abcdefghijklmnopqrstuvwxyz"
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        "0123456789"
-    };
-const int STRING_LENGTH { 12 };
+enum ReturnCode { FILE_OPEN_FAILED = -1, SUCCESS = 0 };
 
-bool verbose { false };
-bool recursive { false };
+const double VERSION{1.4};
+const int RELEASE{1};
+const std::string CHARACTER_SET{"abcdefghijklmnopqrstuvwxyz"
+                                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                "0123456789"};
+const int UID_LENGTH{12};
 
+bool recursive{false};
+bool verbose{false};
+std::vector<std::filesystem::path> file_paths{};
+
+/*
+Iterates through each character in a string of UID_LENGTH size and places a
+random character in its place.
+*/
 std::string generateRandomUID() {
-    std::string result {};
-    result.resize(STRING_LENGTH);
+  std::string result{};
+  result.resize(UID_LENGTH);
 
-    std::random_device random_device {};
-    std::mt19937 random_number_generator(random_device());
-    std::uniform_int_distribution<int> int_distribution (0, static_cast<int>(CHARACTER_SET.length() - 1));
+  std::random_device random_device{};
+  std::mt19937 random_number_generator(random_device());
+  std::uniform_int_distribution<int> int_distribution(
+      0, static_cast<int>(CHARACTER_SET.length() - 1));
 
-    for (int i = 0; i < STRING_LENGTH; i++) {
-        result[i] = CHARACTER_SET[int_distribution(random_number_generator)];
-    }
+  for (int i = 0; i < UID_LENGTH; i++) {
+    result[i] = CHARACTER_SET[int_distribution(random_number_generator)];
+  }
 
-    return result;
+  return result;
 }
 
+// Prints unable to open file error message.
+void printFileErrorMessage(std::string file_path) {
+  std::cout << "ERROR: Unable to open file: " << file_path
+            << "(Maybe invalid read/write permissions?)\n";
+}
+
+/*
+Iterates through each line in a file and writes each line to a temporary
+file, then removes the old file and renames temporary file to the
+name of the old file. If a UID is found within a line, replaces it with a new
+UID using generateRandomUID then writes the line.
+*/
 bool handleFile(std::filesystem::path file_path) {
-    if (file_path.extension() != ".tres") {
-        return true;
+  std::cout << "File: " << file_path.string() << '\n';
+  std::ifstream input_file_stream(file_path);
+
+  if (!input_file_stream.is_open()) {
+    printFileErrorMessage(file_path.string());
+
+    return false;
+  }
+
+  std::filesystem::path tempfile_path(file_path.string() + ".tmp");
+  std::ofstream output_file_stream(tempfile_path);
+
+  if (!output_file_stream.is_open()) {
+    printFileErrorMessage(file_path.string());
+
+    return false;
+  }
+
+  std::string line{};
+  int line_count{};
+
+  while (std::getline(input_file_stream, line)) {
+    size_t uid_position{line.find("\"uid://")};
+
+    if (uid_position == std::string::npos) {
+      output_file_stream << line << '\n';
+
+      continue;
     }
 
-    std::cout << "File: " << file_path.filename() << '\n';
-    std::cout << "Path: " << file_path.parent_path() << '\n';
+    uid_position += 7;
+    size_t endquote_position{line.find('"', uid_position)};
+    std::string new_uid{generateRandomUID()};
 
-    std::ifstream input_file_stream(file_path);
-
-    if (!input_file_stream.is_open()) {
-        std::cerr << "ERROR: Unable to open file: " << file_path << " (Maybe invalid read/write permissions?)\n";
-        return false;
-    }
-    
-    std::filesystem::path tempfile_path(file_path.string() + ".tmp");
-    std::ofstream output_file_stream(tempfile_path);
-
-    if (!output_file_stream.is_open()) {
-        std::cerr << "ERROR: Unable to open temp file: " << tempfile_path << " (Maybe invalid read/write permissions?)\n";
-        return false;
+    if (verbose) {
+      std::cout << "Replacing line: " << line << '\n';
+      std::cout << "[UID: "
+                << line.substr(uid_position, endquote_position - uid_position)
+                << " | New UID: " << new_uid << "]\n";
     }
 
-    std::string line {};
-    int line_count {};
+    line.replace(uid_position, endquote_position - uid_position, new_uid);
 
-    while (std::getline(input_file_stream, line)) {
-        size_t uid_position { line.find("\"uid://") };
+    output_file_stream << line << '\n';
+    line_count++;
+  }
 
-        if (uid_position == std::string::npos) {
-            output_file_stream << line << '\n';
-            continue;
-        }
+  std::cout << "Wrote " << line_count << " line(s).\n";
 
-        uid_position += 7;
-        size_t endquote_position { line.find('"', uid_position) };
+  input_file_stream.close();
+  output_file_stream.close();
 
-        if (verbose) {
-            std::cout << "Replacing line: " << line << '\n';
-            std::cout << "UID: " << line.substr(uid_position, endquote_position - uid_position) << '\n';
-        }
-        
-        line.replace(uid_position, endquote_position - uid_position, generateRandomUID());
+  std::remove(file_path.c_str());
+  std::rename(tempfile_path.c_str(), file_path.c_str());
 
-        if (verbose) {
-            std::cout << "Replaced line: " << line << '\n';
-            std::cout << "UID: " << line.substr(uid_position, endquote_position - uid_position) << '\n';
-        }
-
-        output_file_stream << line << '\n';
-        line_count += 1;
-    }
-
-    std::cout << "Wrote " << line_count << " line(s).\n";
-
-    input_file_stream.close();
-    output_file_stream.close();
-    std::remove(file_path.c_str());
-    std::rename(tempfile_path.c_str(), file_path.c_str());
-
-    return true;
+  return true;
 }
 
+// Checks if the file extension is valid.
+bool checkFileExtension(std::filesystem::path file_path) {
+  if (file_path.extension() == ".tres") {
+    return true;
+  }
 
+  return false;
+}
+
+/*
+Checks if entry is a file and calls checkFileExtension to validate its
+extension. If both are true adds to file_paths vector.
+*/
+void handleDirectoryEntry(const std::filesystem::directory_entry &entry) {
+  if (!entry.is_regular_file()) {
+    return;
+  }
+
+  if (checkFileExtension(entry.path())) {
+    file_paths.push_back(entry.path());
+  }
+}
+
+/*
+Iterates through a directory and if recursive iteration is enabled, its
+subdirectories.
+*/
+void randomizeDirectory() {
+  if (recursive) {
+    std::cout << "Randomizing UIDs of all godot resources in current "
+              << "directory and all subdirectories...\n";
+
+    std::filesystem::recursive_directory_iterator directory_iterator(".");
+    for (const std::filesystem::directory_entry &entry : directory_iterator) {
+      handleDirectoryEntry(entry);
+    }
+  } else {
+    std::cout << "Randomizing UIDs of all godot resources in current "
+              << "directory...\n";
+
+    std::filesystem::directory_iterator directory_iterator(".");
+    for (const std::filesystem::directory_entry &entry : directory_iterator) {
+      handleDirectoryEntry(entry);
+    }
+  }
+}
+
+/*
+Iterates through each file in file_paths and calls handleFile for each one.
+If directory iteration is enabled first calls randomizeDirectory.
+*/
+bool randomize(bool directory = true) {
+  if (directory) {
+    randomizeDirectory();
+  }
+
+  for (const std::filesystem::path &file_path : file_paths) {
+    if (!checkFileExtension(file_path)) {
+      continue;
+    }
+
+    if (!handleFile(file_path)) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 int main(int argc, char **argv) {
-    CLI::App app("Randomizes UIDs of all godot resources in the current directory.");
+  CLI::App app("Randomizes UIDs of godot resources.");
 
-    app.add_flag("-v, --verbose", verbose, "verbosely randomize uids");
-    app.add_flag("-r, --recursive", recursive, "recursively randomize uids");
+  app.add_option("-f, --file", file_paths, "Randomize the specified file(s)")
+      ->check(CLI::ExistingFile);
 
-    argv = app.ensure_utf8(argv);
-    CLI11_PARSE(app, argc, argv);
+  app.add_flag("-r, --recursive", recursive, "Recursively randomize");
+  app.add_flag("-v, --verbose", verbose, "Verbosely randomize");
 
-    std::cout << "godot-uid-fixer v" << VERSION << "\n\n";
+  argv = app.ensure_utf8(argv);
+  CLI11_PARSE(app, argc, argv);
 
-    std::filesystem::path directory_path(".");
+  std::cout << "godot-uid-fixer v" << VERSION << "-" << RELEASE << "\n\n";
 
-    if (recursive) {
-        std::cout << "Randomizing UIDs of all godot resources in current directory and all subdirectories...\n";
-        std::filesystem::recursive_directory_iterator directory_iterator(directory_path);
-        for (const std::filesystem::directory_entry& entry : directory_iterator) {
-            if (entry.is_regular_file()) {
-                if (!handleFile(entry.path())) {
-                  return -2;
-                }
-            }
-        }
-    } else {
-        std::cout << "Randomizing UIDs of all godot resources in current directory...\n";
-        std::filesystem::directory_iterator directory_iterator(directory_path);
-        for (const std::filesystem::directory_entry& entry : directory_iterator) {
-            if (entry.is_regular_file()) {
-                if (!handleFile(entry.path())) {
-                    return -2;
-                }
-            }
-        }
-    }
+  if (!randomize(file_paths.empty())) {
+    return FILE_OPEN_FAILED;
+  }
 
-    std::cout << "done.\n";
-
-    return 0;
+  return SUCCESS;
 }
